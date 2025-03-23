e208bb81fd80584d661aff7c8002e117b8cef562
Vinicius Fernandes <viniciusfernoli@gmail.com>
2025-03-23 00:04:51 -0300

    ajustes

--- .replit.nix
+++ .replit.nix
@@ -1,3 +1,4 @@
+
 { pkgs }: {
   deps = [
     pkgs.chromium
@@ -9,5 +10,15 @@
     pkgs.libwebp
     pkgs.imagemagick
     pkgs.git
+    pkgs.glib
+    pkgs.gtk3
+    pkgs.gsettings-desktop-schemas
+    pkgs.gdk-pixbuf
+    pkgs.cairo
+    pkgs.pango
+    pkgs.atk
+    pkgs.xorg.libX11
+    pkgs.xorg.libxcb
+    pkgs.gobject-introspection
   ];
 }
