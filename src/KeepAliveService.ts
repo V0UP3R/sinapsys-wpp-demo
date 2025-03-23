dba978b59e0a56ea3db6ed13691bb40fc8f83630
Vinicius Fernandes <viniciusfernoli@gmail.com>
2025-03-23 01:07:32 -0300

    ajustes

---
+++ src/KeepAliveService.ts
@@ -0,0 +1,22 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable()
+export class KeepAliveService {
+  private readonly logger = new Logger(KeepAliveService.name);
+
+  constructor(private readonly httpService: HttpService) {}
+
+  @Cron('*/10 * * * * *') // Executa a cada 10 segundos
+  async keepServerAlive() {
+    try {
+      const url = 'http://localhost:3000/'; // Ajuste a URL conforme necessário
+      await lastValueFrom(this.httpService.get(url));
+      this.logger.log(`Keep-alive request sent to ${url}`);
+    } catch (error) {
+      this.logger.error('Failed to send keep-alive request', error);
+    }
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MessageModule } from './message/message.module';
 import { WhatsappModule } from './whatsapp/whatsapp.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { KeepAliveService } from './KeepAliveService';
 
 @Module({
-  imports: [AuthModule, MessageModule, WhatsappModule],
+  imports: [AuthModule, MessageModule, WhatsappModule,ScheduleModule.forRoot()],
+  providers: [KeepAliveService],
 })
 export class AppModule {}
