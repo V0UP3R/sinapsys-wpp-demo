7573975be6d29caf8fb2614a695b8c687e5fc65d
Vinicius Fernandes <viniciusfernoli@gmail.com>
2025-03-23 01:13:11 -0300

    ajustes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { MessageModule } from './message/message.module';
 import { WhatsappModule } from './whatsapp/whatsapp.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { KeepAliveService } from './KeepAliveService';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [AuthModule, MessageModule, WhatsappModule,ScheduleModule.forRoot()],
+  imports: [AuthModule, MessageModule, WhatsappModule,ScheduleModule.forRoot(),HttpModule.register({}),],
   providers: [KeepAliveService],
 })
 export class AppModule {}
